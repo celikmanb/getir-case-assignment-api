e8ba237fa2bd44bb5a8dc94733b0c3a3577f7b4c
celikmanb <celikmanb@mef.edu.tr>
2022-10-19 23:17:16 +0300

    change

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("items.json");
-const middlewares = jsonServer.defaults();
-const port = 3000;
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('items.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
-server.listen(port);
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
