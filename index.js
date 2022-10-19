b9139c01e621c6cb7bf696addd50501efdf5ea1e
celikmanb <celikmanb@mef.edu.tr>
2022-10-19 22:59:44 +0300

    port changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("items.json");
-const port = 80;
+const port = 3000;
 
 server.use(router);
 server.listen(port);
