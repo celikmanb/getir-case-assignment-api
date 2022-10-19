12986aff520078ba1ab160adf3b4971ab839fbce
celikmanb <celikmanb@mef.edu.tr>
2022-10-19 23:02:57 +0300

    fixed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("items.json");
-const port = 3000;
+const port = 80;
 
 server.use(router);
 server.listen(port);
