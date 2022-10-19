1ef6dbf5fd7a62f2fb4b4f17cd9d7b29cd5e53f6
celikmanb <celikmanb@mef.edu.tr>
2022-10-19 23:20:21 +0300

    port add

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('items.json')
 const middlewares = jsonServer.defaults()
-
+const port = process.env.port || 3000
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
